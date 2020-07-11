947566c1ecd7da3acab0474fcff03e1b783778f6
Sean Ransonette <sransonette@gmail.com>
2020-07-11 00:23:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,6 +5,7 @@ def countdown(seconds)
     puts "#{seconds} SECOND(S)!"
     seconds -= 1
   end
+<<<<<<< HEAD
   "HAPPY NEW YEAR!"
 end
 
@@ -12,8 +13,19 @@ end
 
 def countdown_with_sleep(seconds)
   while seconds > 0  
+=======
+"HAPPY NEW YEAR"
+end
+
+def countdown(seconds)
+  while seconds >= 5
+>>>>>>> 44bf4b1a327649707de0d69991a4b024a1dfe753
     puts "#{seconds} SECOND(S)!"
     seconds -= 1
     sleep(1)
   end
+<<<<<<< HEAD
+=======
+"HAPPY NEW YEAR"
+>>>>>>> 44bf4b1a327649707de0d69991a4b024a1dfe753
 end
